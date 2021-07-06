3fb4f7d30ff60e67cbb3a87333b47b810c2f42e2
Alessandro Thea <alessandro.thea@cern.ch>
2021-07-06 14:48:30 +0200

    Adding example script to access to ipbus-example registers

---
+++ test/scripts/test_ipbus_examples.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import ctypes
+import uhal
+import random
+
+ctypes.cdll.LoadLibrary('libwupper-toybox.so')
+
+cm = uhal.ConnectionManager('file://${WUPPER_TOYBOX_SHARE}/config/c.xml', ['ipbusflx-2.0'])
+
+flx0 = cm.getDevice('flx-0-ipb')
+
+for n in flx0.getNodes():
+    print('- ', n)
+
+reg = flx0.getNode('reg').read()
+flx0.dispatch()
+print(f'"reg" current value {hex(reg)}')
+
+print('Generating random number')
+x = random.getrandbits(32)
+print(f'New random value: {hex(x)}')
+
+
+print('Writing')
+flx0.getNode('reg').write(x)
+flx0.dispatch()
+
+print('Reading back')
+reg = flx0.getNode('reg').read()
+flx0.dispatch()
+print(f'"reg" current value {hex(reg)}')
