61f4b09814c92085c971b40eab1e9a0ccb22ac33
Your Name <you@example.com>
2026-09-23 15:19:25 -0500

    JCF: run dbt-clang-format.sh over the files

--- src/formatters.cpp
+++ src/formatters.cpp
@@ -1,65 +1,66 @@
 #include "uhallibs/formatters.hpp"
 
-#include <iomanip>                         // for pair
+#include <iomanip> // for pair
 
 namespace uhallibs {
 
-
-PacketFmt::PacketFmt(const uint8_t* const aPtr, const size_t aNrBytes) :
-  mData(1, std::pair<const uint8_t*, size_t>(aPtr, aNrBytes))
-{}
-
-
-PacketFmt::PacketFmt(const std::vector< std::pair<const uint8_t*, size_t> >& aData) :
-  mData(aData)
-{}
-
-
-PacketFmt::~PacketFmt()
-{}
-
-
-std::ostream& operator<<(std::ostream& aStream, const PacketFmt& aPacket)
+PacketFmt::PacketFmt(const uint8_t* const aPtr, const size_t aNrBytes)
+  : mData(1, std::pair<const uint8_t*, size_t>(aPtr, aNrBytes))
 {
-  std::ios::fmtflags lOrigFlags( aStream.flags() );
+}
+
+PacketFmt::PacketFmt(const std::vector<std::pair<const uint8_t*, size_t>>& aData)
+  : mData(aData)
+{
+}
+
+PacketFmt::~PacketFmt() {}
+
+std::ostream&
+operator<<(std::ostream& aStream, const PacketFmt& aPacket)
+{
+  std::ios::fmtflags lOrigFlags(aStream.flags());
 
   size_t lNrBytesWritten = 0;
   for (size_t i = 0; i < aPacket.mData.size(); i++) {
-    for (const uint8_t* lPtr = aPacket.mData.at(i).first; lPtr != (aPacket.mData.at(i).first + aPacket.mData.at(i).second); lPtr++, lNrBytesWritten++) {
+    for (const uint8_t* lPtr = aPacket.mData.at(i).first;
+         lPtr != (aPacket.mData.at(i).first + aPacket.mData.at(i).second);
+         lPtr++, lNrBytesWritten++) {
       if ((lNrBytesWritten & 3) == 0)
         aStream << std::endl << "   @ " << std::setw(3) << std::dec << (lNrBytesWritten >> 2) << " :  x";
       aStream << std::setw(2) << std::hex << uint16_t(*lPtr) << " ";
     }
   }
 
-  aStream.flags( lOrigFlags );
+  aStream.flags(lOrigFlags);
   return aStream;
 }
 
-DMAFmt::DMAFmt(const uint8_t* const aPtr, const size_t aNrBytes) :
-  mData(1, std::pair<const uint8_t*, size_t>(aPtr, aNrBytes))
-{}
-
-DMAFmt::~DMAFmt()
-{}
-
-std::ostream& operator<<(std::ostream& aStream, const DMAFmt& aPacket)
+DMAFmt::DMAFmt(const uint8_t* const aPtr, const size_t aNrBytes)
+  : mData(1, std::pair<const uint8_t*, size_t>(aPtr, aNrBytes))
 {
-  std::ios::fmtflags lOrigFlags( aStream.flags() );
+}
+
+DMAFmt::~DMAFmt() {}
+
+std::ostream&
+operator<<(std::ostream& aStream, const DMAFmt& aPacket)
+{
+  std::ios::fmtflags lOrigFlags(aStream.flags());
 
   size_t lNrBytesWritten = 0;
   for (size_t i = 0; i < aPacket.mData.size(); i++) {
-    for (const uint8_t* lPtr = aPacket.mData.at(i).first; lPtr != (aPacket.mData.at(i).first + aPacket.mData.at(i).second); lPtr++, lNrBytesWritten++) {
+    for (const uint8_t* lPtr = aPacket.mData.at(i).first;
+         lPtr != (aPacket.mData.at(i).first + aPacket.mData.at(i).second);
+         lPtr++, lNrBytesWritten++) {
       if ((lNrBytesWritten & 63) == 0)
         aStream << std::endl << "   @ " << std::setw(3) << std::dec << (lNrBytesWritten >> 2) << " :  x";
       aStream << std::setw(2) << std::hex << uint16_t(*lPtr) << " ";
     }
   }
 
-  aStream.flags( lOrigFlags );
+  aStream.flags(lOrigFlags);
   return aStream;
 }
-
-
 
 } // namespace uhal
